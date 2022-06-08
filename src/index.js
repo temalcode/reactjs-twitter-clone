75afbc79185aae7c5d8f1ab29eafef4f0a1faa97
@temalcode <79495831+temalcode@users.noreply.github.com>
2022-06-08 16:47:59 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
 import App from "./App";
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(
-  <StrictMode>
+import React from 'react';
+import ReactDOM from 'react-dom';
+ReactDOM.render(
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
